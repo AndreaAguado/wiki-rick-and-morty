abc51951d1309d4c7186fb6367f62fbad41b7184
AndreaAguado <andrea95am@gmail.com>
2021-10-01 18:32:14 +0200

    Publish in GitHub Pages

---
+++ docs/static/js/main.ac411c30.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-starter-kit"]=this["webpackJsonpreact-starter-kit"]||[]).push([[0],{16:function(t,e,r){},23:function(t,e,r){"use strict";r.r(e);r(0);var n=r(8),c=r.n(n),s=r(11),i=(r(16),r(2));var a=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"Hola Mundo!"})})};c.a.render(Object(i.jsx)(s.a,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[23,1,2]]]);
+//# sourceMappingURL=main.ac411c30.chunk.js.map
