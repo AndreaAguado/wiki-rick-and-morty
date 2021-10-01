43c06842aa579cf4493f8f9689a03b7a624f04b3
AndreaAguado <andrea95am@gmail.com>
2021-10-01 18:46:53 +0200

    add and import reset.scss page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import '../styles/App.css';
+import '../styles/App.scss';
+import logo from '../images/Rick_and_Morty_logo.png'
 
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo!</h1>
+    <div className="page">
+      <header className="header">
+        <img src={logo} alt="Rick and Morty logo" />
+      </header>
+      <main className="main">
+        <form action="">
+          <input type="text" />
+        </form>
+      </main>
+      <footer className="footer"></footer>
     </div>
   );
 }
