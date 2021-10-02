7a27367b81ea1ea1e7e6cca763b3fbd2a63284e4
AndreaAguado <andrea95am@gmail.com>
2021-10-02 21:39:37 +0200

    add and link fontsawesome icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ const Footer = () => {
     return (
         <footer className="footer">
             <small className="footer_small"> &copy; Andrea Aguado 2021</small>
-
-        </footer>
+            <a href={'https://github.com/AndreaAguado'} title={'github.com/AndreaAguado'} target="_blank" rel="noreferrer"><i class="fab fa-github github_icon"></i></a>
+        </footer >
     )
 }
 export default Footer;
