447d621eb1c35ad9970f1dc18badc05c545208cf
AndreaAguado <andrea95am@gmail.com>
2021-10-02 21:49:05 +0200

    delete commented code

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,7 +2,6 @@ import '../styles/layout/CharacterCard.scss';
 import { Link } from 'react-router-dom';
 const CharacterCard = (props) => {
     return (
-        // <li key={props.character.id} id={props.character.id} className="character_card">
         <Link to={`/character/${props.character.id}`}>
             <img className="character_img" src={props.character.photo} alt={props.character.name} />
             <div className="character_info">
@@ -10,7 +9,6 @@ const CharacterCard = (props) => {
                 <p className="info_item">{props.character.species}</p>
             </div>
         </Link>
-        // </li >
     )
 }
 export default CharacterCard;
