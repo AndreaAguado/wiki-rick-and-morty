71369bb3c3a27dfea7d05349a166ff92f3f0df13
AndreaAguado <andrea95am@gmail.com>
2021-10-04 20:06:43 +0200

    finish no found page

--- src/components/NoFoundPage.js
+++ src/components/NoFoundPage.js
@@ -1,8 +1,14 @@
 import '../styles/layout/NoFoundPage.scss';
+import noFoundLogo from '../images/404-error-not-found.png';
+import { Link } from 'react-router-dom';
 const NoFoundPage = () => {
     return (
         <section className="nofound_section">
-            <p>Página no encontrada</p>
+            <Link to="/">
+                <p className="infomessage">Volver al Inicio</p>
+            </Link>
+            <img className="img" src={noFoundLogo} alt="Error 404 page not found" />
+
         </section>
     )
 }
