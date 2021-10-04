5975642a249583c8eed53b6b21c267ce5fbadaf2
AndreaAguado <andrea95am@gmail.com>
2021-10-04 18:16:27 +0200

    add status icon to list cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,27 @@
 import '../styles/layout/CharacterCard.scss';
 import { Link } from 'react-router-dom';
 const CharacterCard = (props) => {
+    const classContent = () => {
+        if (props.character.status === 'Alive') {
+            return "fas fa-heartbeat";
+        }
+        else if (props.character.status === 'Dead') {
+            return "fas fa-skull-crossbones";
+        }
+        else {
+            return "fas fa-question";
+        }
+    }
+
     return (
         <Link to={`/character/${props.character.id}`}>
             <img className="character_img" src={props.character.photo} alt={props.character.name} />
-            <div className="character_info">
-                <p className="info_item">{props.character.name}</p>
-                <p className="info_item">{props.character.species}</p>
+            <div className="card_bottom">
+                <div className="character_info">
+                    <p className="info_item">{props.character.name}</p>
+                    <p className="info_item">{props.character.species}</p>
+                </div>
+                <i className={`status_icon ${classContent()}`}></i>
             </div>
         </Link>
     )
