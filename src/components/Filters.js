11b3cd8982581cdd4cc8d8360c8197008e42c4ae
AndreaAguado <andrea95am@gmail.com>
2021-10-04 19:48:49 +0200

    reorder filters in page

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -11,20 +11,19 @@ const Filters = (props) => {
         props.handleStatus(ev.target.value);
     }
     return (
-        <form onSubmit={(ev) => ev.preventDefault()}>
+        <form className="form" onSubmit={(ev) => ev.preventDefault()}>
             <input
                 onKeyUp={handleChange}
                 className="text_input"
-                type="text" />
-            <span className="species_selector_container">
+                type="text"
+                placeholder="Write something here..." />
+            <span className="selector_container">
                 <label htmlFor="species">Species:</label>
                 <select onChange={handleSelect} className="species_filter" name="species" id="species">
                     <option value="All">Show all</option>
                     <option value="Human">Human</option>
                     <option value="Alien">Alien</option>
                 </select>
-            </span>
-            <span className="status_selector_container">
                 <label htmlFor="status">Status:</label>
                 <select onChange={handleSelectStatus} className="status_filter" name="status" id="status">
                     <option value="All">Show all</option>
