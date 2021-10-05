d6768236fdc44c6d7e0eb86a8dc6523536a62d67
AndreaAguado <andrea95am@gmail.com>
2021-10-05 13:18:37 +0200

    use grid in character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,6 +3,25 @@ import ModalWindow from './ModalWindow';
 import logo from '../images/rick_no_found.png';
 
 const CharacterDetail = (props) => {
+    const classContent = () => {
+        if (props.character.status === 'Alive') {
+            return "fas fa-heartbeat";
+        }
+        else if (props.character.status === 'Dead') {
+            return "fas fa-skull-crossbones";
+        }
+        else {
+            return "fas fa-question";
+        }
+    }
+    const classContent2 = () => {
+        if (props.character.species === 'Alien') {
+            return "fas fa-pastafarianism";
+        }
+        else if (props.character.species === 'Human') {
+            return "fas fa-child";
+        }
+    }
     if (props.character === undefined) {
         return (
             <ModalWindow title={'Personaje no encontrado'}>
@@ -15,18 +34,21 @@ const CharacterDetail = (props) => {
             <ModalWindow title={props.character.name}>
                 <article className="character_detail">
                     <img className="character_detail_img" src={props.character.photo} alt={props.character.name} />
-                    <div className="character_info">
-                        <p className="character_info_item">
+                    <div className="character_info_item_1">
+                        <p >
                             Status: {props.character.status} </p>
-                        <p className="character_info_item">
-                            Species: {props.character.species} </p>
-                        <p className="character_info_item">
-                            Origin: {props.character.origin} </p>
-                        <p className="character_info_item">
-                            Episodes: {(props.character.episodes).length} </p>
+                        <i className={`status_icon ${classContent()}`} title={`status: ${props.character.status}`}></i>
                     </div>
+                    <div className="character_info_item_2">
+                        <p >
+                            Species: {props.character.species} </p>
+                        <i className={`status_icon ${classContent2()}`} title={`species: ${props.character.species}`}></i>
+                    </div>
+                    <p className="character_info_item_3">
+                        Origin: {props.character.origin} </p>
+                    <p className="character_info_item_4">
+                        Total episodes: {(props.character.episodes).length} </p>
                 </article>
-                {/* </div > */}
             </ModalWindow>
         )
     }
