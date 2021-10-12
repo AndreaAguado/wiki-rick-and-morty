9b1d4c89e5c2605a0fa7d6ec2be555958e746338
AndreaAguado <andrea95am@gmail.com>
2021-10-12 19:53:37 +0200

    add styles to NavBar buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,9 @@ import '../styles/layout/NavBar.scss';
 const NavBar = (props) => {
     return (
         <nav className="pages_nav_bar">
-            <button className={props.pageNum === 1 ? 'hidden' : ''} onClick={props.handlePrevPage}>Previous</button>
+            <button className={props.pageNum === 1 ? 'hidden' : 'pages_nav_bar_button'} onClick={props.handlePrevPage}>Previous</button>
             <p className="pages_nav_bar_index">Page {props.pageNum} of {props.numberOfPages}</p>
-            <button className={props.pageNum === props.numberOfPages ? 'hidden' : ''} onClick={props.handleNextPage}>Next</button>
+            <button className={props.pageNum === props.numberOfPages ? 'hidden' : 'pages_nav_bar_button'} onClick={props.handleNextPage}>Next</button>
         </nav>
     )
 }
